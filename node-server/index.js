f119f5015ad077b34aefd7fdc7661925afd0dcc8
Pedro Morais <pmor4is@gmail.com>
2023-11-26 23:50:18 -0300

    Adicionando banco de dados de infraestrutura

--- node-server/index.js
+++ node-server/index.js
@@ -139,7 +139,118 @@ app.put("/market/:id", (req,res) => {
   }
 });
 
+
+
+// ROTAS PARA INFRAESTRUTURA
+app.get("/infrastructure", (req, res) => {
+  try {
+    console.log("Query GET requisition");
+    client.query("SELECT * FROM infrastructure", function (error, result) {
+      if (error) {
+        return console.log("Error executing SELECT ALL query from infrastructure. Error: " + error);
+      }
+      res.send(result.rows);
+      console.log("Query GET executed successfully");
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.get("/infrastructure/:id", (req,res) => {
+  try {
+    console.log("Query GET/" + req.params.id + " requisition");
+    client.query(
+      "SELECT * FROM infrastructure WHERE id = $1", 
+      [req.params.id],
+      function (error, result) {
+        if (error) {
+          return console.log("Error executing SELECT ${id} query from infrastructure. Error: " + error);
+        }
+        if (result.rowCount == 0) {
+          res.send("User not found");
+        } else {
+          res.send(result.rows);
+          console.log("Query GET/" + req.params.id + " successfully");
+        }
+      }
+    );
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.delete("/infrastructure/:id" , (req,res) => {
+  try {
+    console.log("Query DELETE/" + req.params.id + " requisition");
+    const id = req.params.id;
+    client.query(
+      "DELETE FROM infrastructure WHERE id = $1",
+      [id],
+      function(error, result) {
+        if (error) {
+          return console.log("Error executing DELETE ${id} query. Error: " + error);
+        } else {
+          if (result.rowCount == 0) {
+            res.status(400).json({ info: "User not found" });
+          } else {
+            res.status(200).json({ info: "User successfully deleted" });
+          }
+        }
+        console.log("Query DELETE successfully");
+      }
+    )
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.post("/infrastructure", (req,res) => {
+  try {
+    console.log("Query POST requisition", req.body);
+    const { repairName, repairDescription, repairLocal, repairPriority, repairObservations, repairLimitDate } = req.body;
+    client.query(
+      "INSERT INTO infrastructure (repairName, repairDescription, repairLocal, repairPriority, repairObservations, repairLimitDate) VALUES ($1, $2, $3, $4, $5, $6) RETURNING * ",
+      [repairName, repairDescription, repairLocal, repairPriority, repairObservations, repairLimitDate],
+      function (error, result) {
+        if (error) {
+          return console.log("Error executing INSERT query. Error: " + error);
+        } 
+        const { id } = result.rows[0];
+        res.setHeader("id", '${id}');
+        res.status(201).json(result.rows[0]);
+        console.log("Query INSERT successfully.");
+      }
+    );
+  } catch (error) {
+    console.log(error);
+  } 
+});
+
+app.put("/infrastructure/:id", (req,res) => {
+  try {
+    console.log("Query PUT requisition", req.body);
+    const id = req.params.id;
+    const { repairName, repairDescription, repairLocal, repairPriority, repairObservations, repairLimitDate } = req.body;
+    client.query(
+      "UPDATE infrastructure SET repairName=$1, repairDescription=$2, repairLocal=$3, repairPriority=$4, repairObservations=$5, repairLimitDate=$6 WHERE id=$7",
+      [repairName, repairDescription, repairLocal, repairPriority, repairObservations, repairLimitDate, id],
+      function (error, result) {
+        if (error) {
+          return console.log("Error executing UPDATE query. Error: " + error);
+        } else {
+          res.setHeader("id", id);
+          res.status(202).json({ identifier: id });
+          console.log("Query UPDATE successfully.");
+        }
+      }
+    )
+  } catch (error) {
+    console.log(error);
+  }
+});
+
 app.listen(config.port, () =>
   console.log("Servidor funcionando na porta " + config.port)
 );
-// module.exports = app;
+module.exports = app;
